d4a890d8e6b3fa49fb5605119c8bacd70c938d81
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-03-20 12:20:38 +0100

    .

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var stringifyFile = 'req.params.note';
+
+app.use(bodyParser.json()); /* wykorzystuje middleware body-parser*/
+
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    stringifyFile = data
+    res.send(data);
+})
+});
+
+app.post('/updateNote/:note', function (req, res) {
+    fs.writeFile('./test.json', stringifyFile, function(err) {
+   if (err) throw err;
+    console.log('file updated');
+})
+});
+
+
+app.listen(3000);
