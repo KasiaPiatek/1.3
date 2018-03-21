66381f5487f7314fafccbf3f56000ce04a1d9680
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-03-21 12:41:42 +0100

    .

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var Parser = require('bodyParser');
 var stringifyFile = 'req.params.note';
 
 app.use(bodyParser.json()); /* wykorzystuje middleware body-parser*/
 
 
-app.get('/getNote', function(req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
+app.get('/getNote', function (req, res) {
+    console.log('Otrzymałem żądanie GET do strony głównej');
+    res.send('Hello GET!');
+});
+
+fs.readFile('./test.json', 'utf8', function(err, data) {
     if (err) throw err;
     stringifyFile = data
     res.send(data);
-})
 });
 
 app.post('/updateNote/:note', function (req, res) {
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-   if (err) throw err;
-    console.log('file updated');
-})
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST!');
 });
 
+fs.writeFile('./test.json', stringifyFile, function(err) {
+    If (err) throw err;
+    console.log('file updated');
+});
 
 app.listen(3000);
